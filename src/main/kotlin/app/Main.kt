8e977ad37bd68bdfdfd61be3ada6a4ab67554f65
pim-t <piraya.qt@gmail.com>
2021-05-12 23:08:07 +1000

    communicating test

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -2,7 +2,11 @@ package app
 import io.javalin.Javalin
 import com.mongodb.BasicDBObject
 import com.mongodb.MongoClient
+import org.bson.Document
 import org.eclipse.jetty.server.Authentication
+import org.json.JSONObject
+import org.litote.kmongo.MongoOperator
+import org.litote.kmongo.jsonSchema
 
 const val JAVALIN_PORT = 7000
 const val MONGO_PORT = 27017
@@ -15,17 +19,31 @@ fun main() {
     app.get("/") { ctx -> ctx.result("Hello World") }
 
     app.ws("/") { ws ->
-        ws.onConnect { ctx -> println("Connected") }
+        var mongoClient: MongoClient? = null
+        mongoClient = MongoClient("localhost", MONGO_PORT)
+        ws.onConnect { ctx -> println("Connected")
+            var db = mongoClient.getDB("Documents")
+            var tbl = db.getCollection("doc_contents")
+            var document = BasicDBObject()
+            document.put("title", "testing")
+
+            var cursor = tbl.find(document)
+            var it = cursor.iterator()
+            while (it.hasNext()) {
+                print(it.next())
+            }
+            ctx.send("""{"name":"test name", "age":25}""")
+        }
         ws.onMessage { ctx ->
                 print(ctx.message())
-                var mongoClient: MongoClient? = null
-                mongoClient = MongoClient("localhost", MONGO_PORT)
 
-                var db = mongoClient.getDB("testDB")
-                var tbl = db.getCollection("user")
+                var db = mongoClient.getDB("Documents")
+                var tbl = db.getCollection("doc_contents")
                 val document = BasicDBObject()
                 document.put("name",ctx.message())
                 tbl.insert(document)
+
+
         }
     }
 }
